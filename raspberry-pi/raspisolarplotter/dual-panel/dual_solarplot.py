cc728019f1b6d2ed03401fc98e3be45afd480b73
Jacob David C Cunningham <greenace92@gmail.com>
2018-03-07 14:49:27 -0600

    removed semi colons

--- raspberry-pi/raspisolarplotter/dual-panel/dual_solarplot.py
+++ raspberry-pi/raspisolarplotter/dual-panel/dual_solarplot.py
@@ -30,9 +30,9 @@ cur_date_day = cur_date.strftime('%A')
 key = ''
 
 # get analog value and calculate based on 5V multiplier
-multiplier = 0;
-reference = 5;
-steps = 1024;
+multiplier = 0
+reference = 5
+steps = 1024
 
 def plot_calc_send_req(pin_num):
 
